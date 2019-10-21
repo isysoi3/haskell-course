cfef8644169f8305a35962973a28a7594868178f
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-10-21 16:13:28 +0300

    some code refacrotred

--- huffman.hs
+++ huffman.hs
@@ -14,6 +14,7 @@ compareTrees t1 t2
  | (getFrequency t1) > (getFrequency t2) = GT
  | otherwise = EQ
 
+
 toString :: Show a => HuffmanTree a -> String
 toString (Leaf v p) = (show v) ++ "--" ++ (show p)
 toString (Node p l r) = "< " ++ (toString l) ++ " | " ++ (show p) ++ " | " ++ (toString r) ++ " >" 
@@ -36,12 +37,17 @@ sortHuffmanTrees xs = sortBy compareTrees xs
             
 
 toHuffmanTree :: Ord a => [a] -> HuffmanTree a
-toHuffmanTree str = toHuffmanTree_helper $ toLeafs str
+toHuffmanTree str = toHuffmanTree_helper $ sortHuffmanTrees $ toLeafs str
 toHuffmanTree_helper (t:[]) = t
 toHuffmanTree_helper (t1:t2:xs) = case compareTrees t1 t2 of
-    LT -> toHuffmanTree_helper (sortHuffmanTrees (((Node (getFrequency t1 + getFrequency t2) t1 t2)):xs))
     GT -> toHuffmanTree_helper (sortHuffmanTrees (((Node (getFrequency t1 + getFrequency t2) t2 t1)):xs))
-    EQ -> toHuffmanTree_helper (sortHuffmanTrees (((Node (getFrequency t1 + getFrequency t2) t1 t2)):xs))
+    otherwise -> toHuffmanTree_helper (sortHuffmanTrees (((Node (getFrequency t1 + getFrequency t2) t1 t2)):xs))
     
+fromHuffmanTreeToMap :: Ord a => HuffmanTree a -> [(a, [Bool])]
+fromHuffmanTreeToMap_helper xs (Leaf v p) = [(v, (reverse xs))]
+fromHuffmanTreeToMap_helper xs (Node p l r) = (fromHuffmanTreeToMap_helper (True:xs) l) ++ (fromHuffmanTreeToMap_helper (False:xs) r)
+fromHuffmanTreeToMap = fromHuffmanTreeToMap_helper []
 
--- map char : Int 
+encode :: [a] -> [Bool]
+encode str =  toHuffmanTree str
+map char : Int 
